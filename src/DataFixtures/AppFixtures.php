b56afd54abea02c0f9be347cde31923c18dd0d6e
max <ren.maxime@gmail.com>
2020-04-29 16:12:47 +0200

    change entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,19 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Framework;
+use App\Entity\Level;
+use App\Entity\Program;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Validator\Constraints\Length;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+       /*  $faker = \Faker\Factory::create();*/
         $manager->flush();
     }
 }
